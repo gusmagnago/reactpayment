810302553faeba1e9afe503b2cee9630282dd72e
Gustavo <gusmagnago@gmail.com>
2021-03-09 16:43:14 +0000

    express config created to essentially work on production and scripts to be sure that heroku is deploying the build

--- index.js
+++ index.js
@@ -30,7 +30,18 @@ app.use(passport.session());
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
 
+//express config to essentially work on production
+if (process.env.NODE_ENV === "production") {
+  //express will serv up production assets
+  //like our main.js file or main.css file client/build
+  app.use(express.static("client/build"));
+  //Express will serve up the index.html file
+  //if it doesnt recognize the route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
-
-//http://localhost:5000/
